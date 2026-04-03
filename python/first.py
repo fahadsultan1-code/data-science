4ff46d026059b1cfb7a6a669ab3846d425e2a44f
musaddiqm172@gmail.com <musaddiqm172@gmail.com>
2026-04-04 04:59:52 +0500

    ading new code

--- python/first.py
+++ python/first.py
@@ -1,1 +1,69 @@
 print("hi welcome to data science journey")
+name="fahad sultan"
+age=23
+price=22.2
+print("my name is :",name)
+print("my age is :",age)
+print("the price is :",price)
+ #sum code
+a = 20 
+b = 30
+c = a+b
+print("a=",a)
+print("b=",b)
+print("sum=",c)
+#arthmatic opreators
+a = 20 
+b = 30
+c = a+b
+d = a-b
+e = a*b
+f = a/b 
+g =a % b
+
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(c**a)
+#relational or comaprasion operators it will return only true or false 
+print(a==a)
+print(a==b) #equal to
+print(a!=b)#not equal to
+print(a<b)#less then to
+print(a>b)#greater then to
+print(a<=b)#less then or equal to
+print(a>=b)#greater then or equal to
+#assignment opreator
+num=10
+num+=10 #add a new value to same variable
+#num-=10 #sub a new value to same variable
+#num/=10 #devide a new value to same variable
+#num*=10 #mul a new value to same variable
+#num%=10 #modlu a new value to same variable
+num**=10 #power mul a new value to same variable
+print(num)
+#logicl opreators {  not  and or } not can change status , and opreator show true only when both are true
+num1= 5
+num2=10
+print(not (num1<num2))
+val1=True
+val2=True
+print(val1 and val2)
+#or show true when 1 value is true
+val1=True
+val2=False
+print(val1 or val2)
+
+
+#type conversion it change the type of variable
+ 
+val3=12.2
+val3=str(val3)
+print(type(val3))
+ 
+ 
+
+
+
