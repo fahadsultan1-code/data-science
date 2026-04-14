1c8424f233779fabec7fa6073aba38574f1718af
musaddiqm172@gmail.com <musaddiqm172@gmail.com>
2026-04-14 13:17:00 +0500

    ading new code

--- python/practice.py
+++ python/practice.py
@@ -1,4 +1,18 @@
+#a=int(input("enter 1st number: "))
+#b=int(input("enter 2nd number:"))
+#sum= a+b
+#print("the sum is :",sum)
+
 a=int(input("enter 1st number: "))
-b=int(input("enter 2nd number:"))
-sum= a+b
-print("the sum is :",sum)
+a+=10 #add a new value to same variable
+print(a)
+a-=10 #sub a new value to same variable
+print(a)
+a/=10 #devide a new value to same variable
+print(a)
+a*=10 #mul a new value to same variable
+print(a)
+a%=10 #modlu a new value to same variable
+print(a)
+a**=10
+print(a)
