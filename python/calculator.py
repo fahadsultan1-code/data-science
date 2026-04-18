9684b207647560af7b135679aabaef3639614c90
musaddiqm172@gmail.com <musaddiqm172@gmail.com>
2026-04-18 13:05:23 +0530

    ading new code

---
+++ python/calculator.py
@@ -0,0 +1,22 @@
+while True:
+
+    a=int(input("enter first number :"))
+    b=int(input("enter second number:"))
+    print("press + for addition")
+    print("press - for addition")
+    print("press - for addition")
+    print("press / for addition")
+    c=input("enter opreator:")
+
+    if c=="+":(
+        print(a+b)
+    )
+    if c=="-":(
+        print(a-b)
+    )
+    if c=="*":(
+        print(a*b)
+    )
+    if c=="/":(
+        print(a/b)
+    )
